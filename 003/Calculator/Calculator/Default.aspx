387dbed806cf9ddddc6572e05419b86568d56cd9
waryzh <waryzh@gmail.com>
2014-07-22 12:13:19 -0400

    submit homework 003

---
+++ 003/Calculator/Calculator/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Calculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Calculator</h1>
+            <asp:TextBox runat="server" ID="tb1" /><br />
+            <asp:TextBox runat="server" ID="tb2" /><br />
+            <asp:TextBox runat="server" ID="tb3" /><br />
+            <asp:Button runat="server" Text="Calculate" OnClick="Calculate" />
+            <select runat="server" id="selCal">
+                <option value="add">Add</option>
+                <option value="sub">Sub</option>
+                <option value="mul">Mul</option>
+                <option value="div">Div</option>
+            </select>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 003/Calculator/Calculator/Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void Calculate(object sender, EventArgs e)
+        {
+            double a = double.Parse(this.tb1.Text);
+            double b = double.Parse(this.tb2.Text);
+            double result = 0;
+            string calc = this.selCal.Value;
+            if (calc.Equals("add"))
+            {
+                result = a + b;
+            }
+            else if (calc.Equals("sub"))
+            {
+                result = a - b;
+            }
+            else if (calc.Equals("mul"))
+            {
+                result = a * b;
+            }
+            else
+            {
+                result = a / b;
+            }
+            this.tb3.Text = result.ToString();
+        }
+    }
+}
